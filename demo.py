4467de31efbd908361291c95277571e8bd7cb0f7
unknown <foamliu@yeah.net>
2018-11-02 18:08:30 +0800

    update

--- demo.py
+++ demo.py
@@ -1,8 +1,3 @@
-# 短文本相似度
-
-## 代码实现
-
-<pre>
 import difflib
 
 # 其中的str1，str2并未分词，是两组字符串
@@ -11,10 +6,3 @@ str2 = '兆丰世贸大厦'
 
 diff_result = difflib.SequenceMatcher(None, str1, str2).ratio()
 print(diff_result)
-</pre>
-
-## 执行结果
-
-<pre>
-0.8333333333333334
-</pre>
